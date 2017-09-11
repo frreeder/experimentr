e2f0c22c87059a2044807a33eef2fb49f7933ffe
Alyssa Pena <mupena17@tamu.edu>
2017-09-11 11:38:39 -0500

    There is now a boolean on graphInteract for the purpose of gathering the pictures of the charts. Also added vertical lines at end of bars. Will now move onto graphThemes.

--- public/modules/graphQuestions/getInteractNames.py
+++ public/modules/graphQuestions/getInteractNames.py
@@ -31,18 +31,16 @@ for x in fileNames:
                                 j["imagePath"].append("modules/graphQuestions/graphImages/interact/"+x)
 
 # pie
-for x in fileNames:
-    if "pie" in x:
-        for i in data["pie"]:
-            for j in data["pie"][i]:
-                # print("j", j)
-                    if j["key"] in x:
-                        for ind, k in enumerate(j["imgPath"]):
-                            if k in x:
-                                print ("iCaught: ", x)
-                                # might not need iconDim
-                                img = cv2.imread("./graphImages/interact/"+x,1)
-                                j["imagePath"].append({"path": "modules/graphQuestions/graphImages/interact/"+x, "iconDim": (img.shape[1], img.shape[0])})
+for i in data["pie"]:
+    for j in data["pie"][i]:
+        # print("j", j)
+        for ind, k in enumerate(j["imgPath"]):
+            for x in fileNames:
+                if "pie" in x and j["key"] in x and k in x:
+                    print ("iCaught: ", x)
+                    # might not need iconDim
+                    img = cv2.imread("./graphImages/interact/"+x,1)
+                    j["imagePath"].append({"path": "modules/graphQuestions/graphImages/interact/"+x, "iconDim": (img.shape[1], img.shape[0])})
 
 for j in data["pie"]:
     print ("j", j)
@@ -51,21 +49,20 @@ for j in data["pie"]:
         del k["imgPath"]
 
 # bar
-for x in fileNames:
-    if "bar" in x:
-        for i in data["bar"]:
-            for j in data["bar"][i]:
-                    if j["key"] in x:
-                        for ind, k in enumerate(j["imgPath"]):
-                            if k in x:
-                                print ("iCaught: ", x)
-                                if ind == 0:
-                                    img = cv2.imread("./graphImages/interact/"+x,1)
-                                    j["imageDim"]= (img.shape[1], img.shape[0])
-                                if ind == 1:
-                                    img = cv2.imread("./graphImages/interact/"+x,1)
-                                    j["barDim"]= (img.shape[1], img.shape[0])
-                                j["imagePath"].append("modules/graphQuestions/graphImages/interact/"+x)
+for i in data["bar"]:
+    for j in data["bar"][i]:
+        for ind, k in enumerate(j["imgPath"]):
+            for x in fileNames:
+                if j["key"] in x and "bar" in x and k in x:
+                    print ("iCaught: ", x)
+                    if ind == 0:
+                        img = cv2.imread("./graphImages/interact/"+x,1)
+                        j["imageDim"]= (img.shape[1], img.shape[0])
+                    if ind == 1:
+                        img = cv2.imread("./graphImages/interact/"+x,1)
+                        j["barDim"]= (img.shape[1], img.shape[0])
+                    j["imagePath"].append("modules/graphQuestions/graphImages/interact/"+x)
+                    continue
 
 for j in data["bar"]:
     print ("j", j)
